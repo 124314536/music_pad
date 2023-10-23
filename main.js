3f87b1eb331b9cb5c1d009c08b0f72bb41e519e3
124314536 <133114140+124314536@users.noreply.github.com>
2023-10-23 08:21:57 -0300

    index.html

--- main.js
+++ main.js
@@ -3,8 +3,6 @@ function tocaSomPom () {
     document.querySelector('#som_tecla_Pom').play();
 }
 
-document.querySelector('.tecla_Pom').onclick = tocaSomPom;
-
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 
