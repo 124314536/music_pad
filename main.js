cea0a195be433e5ddd8a8a134d8eea0d2ed512c8
124314536 <133114140+124314536@users.noreply.github.com>
2023-11-06 08:42:47 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,19 +1,8 @@
-
-function tocaSom (idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+// Pom ----------------------------------------
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
 }
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0;
-
-//enquato
-while (contador < listaDeTeclas.length) {
-
-  listaDeTeclas[contador].onclick = tocaSom;
-
-  contador = contador +1;
-
-  console.log(contador)
-
-}
+listaDeTeclas[0].onclick = tocaSomPom;
